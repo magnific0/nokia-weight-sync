e8de6e4be105a8a75a306af601af05a130017583
Jacco Geul <jacco@geul.net>
2018-02-21 10:27:30 +0100

    Add boilerplate code https://www.python-boilerplate.com/py3+executable+argparse

---
+++ nokia-to-garmin-weight.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Nokia Health to Garmin Connect Weight Updater
+"""
+
+__author__ = "Jacco Geul"
+__version__ = "0.1.0"
+__license__ = "GPLv3"
+
+import argparse
+
+
+def main(args):
+    """ Main entry point of the app """
+    print("hello world")
+    print(args)
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument("arg", help="Required positional argument")
+
+    # Optional argument flag which defaults to False
+    parser.add_argument("-f", "--flag", action="store_true", default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    parser.add_argument("-n", "--name", action="store", dest="name")
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)
