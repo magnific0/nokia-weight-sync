c91c36b92e23196204aeaa10bcffd8e69c75c996
Jacco Geul <jacco@geul.net>
2018-08-20 23:13:31 +0200

    Update access_token get parameter after refresh.

--- nokia.py
+++ nokia.py
@@ -43,7 +43,7 @@ import datetime
 import json
 
 from arrow.parser import ParserError
-from requests_oauthlib import OAuth2Session
+from requests_oauthlib import OAuth2Session, TokenUpdated
 
 class NokiaCredentials(object):
     def __init__(self, access_token=None, token_expiry=None, token_type=None,
@@ -124,8 +124,7 @@ class NokiaApi(object):
         self.client = OAuth2Session(credentials.consumer_key, 
                                     token=self.token, 
                                     auto_refresh_url=refresh_url, 
-                                    auto_refresh_kwargs=extra, 
-                                    token_updater=self.set_token)
+                                    auto_refresh_kwargs=extra)
         
     def get_credentials(self):
         return self.credentials
@@ -146,7 +145,13 @@ class NokiaApi(object):
             if is_date(key) and is_date_class(val):
                 params[key] = arrow.get(val).timestamp
         url_parts = filter(None, [self.URL, version, service])
-        r = self.client.request(method, '/'.join(url_parts), params=params)
+        request_url = '/'.join(url_parts)
+        try:
+            r = self.client.request(method, request_url, params=params)
+        except TokenUpdated as e:
+            self.set_token(e.token)
+            params['access_token'] = self.token['access_token']
+            r = self.client.request(method, request_url, params=params)
         response = json.loads(r.content.decode())
         if response['status'] != 0:
             raise Exception("Error code %s" % response['status'])
